5b56b1327381846f1015017162eaeb410892a5c0
priyash <priyashbarman9@gmail.com>
2024-03-29 22:29:28 +0530

    authorization-added

--- app/routes/role_routes.py
+++ app/routes/role_routes.py
@@ -1,5 +1,7 @@
 from flask import Blueprint
+from flask_login import login_required
 
+from app.auth import role_required
 from app.controllers import RoleController
 
 role_bp = Blueprint("role_bp", __name__)
@@ -7,10 +9,14 @@ role_controller = RoleController()
 
 
 @role_bp.route("/admin/roles/")
+@login_required
+@role_required([1, 2])
 def index():
     return role_controller.get()
 
 
 @role_bp.route("/admin/roles/add/", methods=["GET", "POST"])
+@login_required
+@role_required([1])
 def add():
     return role_controller.create()
