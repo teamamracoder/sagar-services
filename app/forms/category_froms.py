6e14c523022265c4476d834fc7e6465e4f58840a
protap_barman <protapbarman02@gmail.com>
2024-03-30 16:39:55 +0530

    category panel image upload, added products/img directory to gitignore

--- app/forms/category_froms.py
+++ app/forms/category_froms.py
@@ -1,12 +1,13 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField
 from wtforms.validators import DataRequired
-
+from flask_wtf.file import FileField, FileRequired
 
 class CreateCategoryForm(FlaskForm):
-    category_name = StringField("category_name", validators=[DataRequired()])
-    category_img_url = StringField("category_img_url")
+    category_name = StringField("Name", validators=[DataRequired()])
+    category_img_url = FileField("Image",validators=[FileRequired()])
+
 
 class UpdateCategoryForm(FlaskForm):
-    category_name = StringField("category_name", validators=[DataRequired()])
-    category_img_url = StringField("category_img_url")
+    category_name = StringField("Name", validators=[DataRequired()])
+    category_img_url = FileField("Image")
