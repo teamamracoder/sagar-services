e957c3cd95167a6560fa48b9e459ea54bd4a4fa1
priyash <priyashbarman9@gmail.com>
2024-03-31 23:55:22 +0530

    updated

--- app/controllers/user_controller.py
+++ app/controllers/user_controller.py
@@ -1,11 +1,12 @@
 from flask import render_template, redirect, url_for, request, jsonify
 from app.forms import CreateUserForm
-from app.services import UserService
+from app.services import UserService, RoleService
 
 
 class UserController:
     def __init__(self) -> None:
         self.user_service = UserService()
+        self.role_service = RoleService()
 
     def create(self):
         form = CreateUserForm()
@@ -27,4 +28,5 @@ class UserController:
         # Determine the column to sort by
         columns = ["id", "first_name", "email"]
         data = self.user_service.get(request, columns)
-        return jsonify(data)
+        user_data = self.role_service.add_roles_with_users(data)
+        return jsonify(user_data)
--- app/services/role_service.py
+++ app/services/role_service.py
@@ -1,8 +1,23 @@
 from db import db
 from app.models import RoleModel
 from .base_service import BaseService
+from app.constants import roles
 
 
 class RoleService(BaseService):
     def __init__(self) -> None:
         super().__init__(RoleModel)
+
+    def add_roles_with_users(self, users: list) -> list:
+        for user in users["data"]:
+            user["roles"] = self.get_roles_by_user_id(user["id"])
+        return users
+
+    def get_roles_by_user_id(self, user_id: int) -> list:
+        query = (
+            RoleModel.query.filter_by(user_id=user_id)
+            .with_entities(RoleModel.role)
+            .all()
+        )
+        role_list = [roles.get_value(row[0]) for row in query]
+        return role_list
