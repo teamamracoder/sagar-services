f87f8a749601009f642b067fae73192b009660c4
protap_barman <protapbarman02@gmail.com>
2024-04-06 21:09:31 +0530

    separated QnA

--- app/services/product_question_service.py
+++ app/services/product_question_service.py
@@ -4,3 +4,12 @@ from .base_service import BaseService
 class ProductQuestionService(BaseService):
     def __init__(self) -> None:
         super().__init__(ProductQuestionModel)
+
+    def add_question_with_this(self, datas:dict)->list:
+        for data in datas["data"]:
+            question=self.get_question_details_by_question_id(data["question_id"])
+            data["question"] = question.question
+        return datas
+    
+    def get_question_details_by_question_id(self, question_id):
+        return ProductQuestionModel.query.filter_by(id=question_id).first()
