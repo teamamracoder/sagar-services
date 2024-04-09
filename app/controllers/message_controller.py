8b3b96ac231a0fd60fe07086cd3ec910d26f4c1c
protap_barman <protapbarman02@gmail.com>
2024-04-10 01:19:49 +0530

    no way, dashboard layout

--- app/controllers/message_controller.py
+++ app/controllers/message_controller.py
@@ -13,9 +13,12 @@ class MessageController:
 
     def get(self,conversation_id):
         form = CreateMessageForm()
+        conversation=self.conversation_service.get_by_id(conversation_id)
+        user_id=conversation.user_id
+        print(user_id)
         messages=self.message_service.get_by_conversation_id(conversation_id)
         messages=self.user_service.add_message_with_this(messages)
-        return render_template("admin/message/index.html",messages=messages,conversation_id=conversation_id, form=form)
+        return render_template("admin/message/index.html",messages=messages,conversation_id=conversation_id, form=form, user_id=user_id)
     
     
     def create(self,conversation_id):
