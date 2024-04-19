67dae34c3d24b00468c0649b4810bc5bedfdd989
SubhasisBarman <subhasisbarman9288@gmail.com>
2024-04-19 12:48:57 +0530

    Home page Modification

--- app/static/js/subhasis.js
+++ app/static/js/subhasis.js
@@ -1,10 +1,11 @@
 //NavBar
-function showDropdown() {
-  document.getElementById("dropdownMenu").style.display = "block";
-}
 
-function hideDropdown() {
-  document.getElementById("dropdownMenu").style.display = "none";
+function toggleDropdown(container) {
+  if (window.innerWidth <= 991) {
+    container.classList.toggle('active');
+    var bottomNavItems = document.querySelector('.bottomNavItems');
+    bottomNavItems.classList.toggle('active');
+  }
 }
 
 //NavBar
@@ -19,44 +20,44 @@ $('.owl-carousel-Product').owlCarousel({
   // autoplayTimeout: 3000,
   autoplayHoverPause: true,
   responsive: {
-      0: {
-          items: 1
-      },
-      360: {
-          items: 1.9
-      },
-      550: {
-          items: 2.5
-      },
-      750: {
-          items: 3.5
-      },
-      850: {
-          items: 4
-      },
-      950: {
-          items: 4.5
-      },
-      1150: {
-          items: 5.5
-      }
+    0: {
+      items: 1
+    },
+    360: {
+      items: 1.9
+    },
+    550: {
+      items: 2.5
+    },
+    750: {
+      items: 3.5
+    },
+    850: {
+      items: 4
+    },
+    950: {
+      items: 4.5
+    },
+    1150: {
+      items: 5.5
+    }
   }
-})
+
+});
+
 
 // For ProductName
 document.addEventListener("DOMContentLoaded", function () {
   var ProductNames = document.querySelectorAll(".ProductName");
   ProductNames.forEach(function (ProductName) {
 
-      var originalText = ProductName.innerHTML;
-      var maxLength = 52;
-      var originalLength = originalText.length;
-      console.log(originalLength);
-      if (originalLength > maxLength) {
-          console.log(originalText.length);
-          var truncatedText = originalText.slice(0, 52) + "...";
-          ProductName.textContent = truncatedText;
-      }
+    var originalText = ProductName.innerHTML;
+    var maxLength = 52;
+    var originalLength = originalText.length;
+    if (originalLength > maxLength) {
+      var truncatedText = originalText.slice(0, 52) + "...";
+      ProductName.textContent = truncatedText;
+    }
   });
 
 });
@@ -70,24 +71,24 @@ $('.vendor-carousel').owlCarousel({
   autoplay: true,
   smartSpeed: 3000,
   responsive: {
-      0: {
-          items: 2.5
-      },
-      576: {
-          items: 3.5
-      },
-      700: {
-          items: 4.5
-      },
-      768: {
-          items: 5.5
-      },
-      992: {
-          items: 6.5
-      },
-      1200: {
-          items: 7.5
-      }
+    0: {
+      items: 2.5
+    },
+    576: {
+      items: 3.5
+    },
+    700: {
+      items: 4.5
+    },
+    768: {
+      items: 5.5
+    },
+    992: {
+      items: 6.5
+    },
+    1200: {
+      items: 7.5
+    }
   }
 });
 
@@ -107,7 +108,7 @@ function showProductCarousel(productToShow, filterClass) {
   // Remove 'active' class from all filters
   var allProductType = document.querySelectorAll('.ProductCategory');
   allProductType.forEach(filter => {
-      filter.classList.remove('active');
+    filter.classList.remove('active');
   });
 
   // Add 'active' class to the selected filter
@@ -131,19 +132,26 @@ document.getElementById("BestSellingFilter").addEventListener("click", function 
 // For ScrollBar
 function scrollToTop() {
   window.scrollTo({
-      top: 0,
-      behavior: 'smooth'
+    top: 0,
+    behavior: 'smooth'
   });
 }
 
 let scrollDiv = document.getElementById("scrollDiv");
 window.onscroll = function () {
   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-      scrollDiv.style.display = "block";
+    scrollDiv.style.display = "block";
 
   } else {
-      scrollDiv.style.display = "none";
+    scrollDiv.style.display = "none";
   }
 };
 
 // ScrollBar End
+
+// For tooltip
+
+const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+
+// For tooltip End
