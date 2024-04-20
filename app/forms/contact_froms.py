1fb241933fb4dfd8baa81a58e82b8678e8b3dcee
SubhasisBarman <subhasisbarman9288@gmail.com>
2024-04-20 23:49:05 +0530

    Update Contact

--- app/forms/contact_froms.py
+++ app/forms/contact_froms.py
@@ -1,15 +1,15 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField,FileField
+from wtforms import StringField,FileField,TextAreaField
 from wtforms.validators import DataRequired, Email
-from flask_wtf.file import FileAllowed, FileRequired
+from flask_wtf.file import FileAllowed,MultipleFileField
 
 
 class CreateContactForm(FlaskForm):
     name=StringField("Name",validators=[DataRequired()])
     email = StringField("Email", validators=[DataRequired(), Email()])
     phone = StringField("Phone", validators=[DataRequired()])
-    query_message=StringField("Query Message",validators=[DataRequired()])
-    query_img_urls=FileField("Query Images", validators=[FileRequired(),FileAllowed(['jpg', 'png', 'jpeg', 'webp','gif'])])
+    query_message=TextAreaField("Query Message",validators=[DataRequired()])
+    query_img_urls=MultipleFileField("Query Images", validators=[FileAllowed(['jpg', 'png', 'jpeg', 'webp','gif'])])
 
 
 class UpdateContactForm(FlaskForm):
