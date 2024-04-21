1bd8d63488417234c422c50aae61c34c6afd71ab
protap_barman <protapbarman02@gmail.com>
2024-04-22 03:06:08 +0530

    background image

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 class Config:
-    SQLALCHEMY_DATABASE_URI = "postgresql://postgres:2023@localhost/temp_db_vv"
+    SQLALCHEMY_DATABASE_URI = "postgresql://postgres:2024@localhost/temp_db_vv"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = "Wfz9i5MPukvNRYbXlDDaXw=="
