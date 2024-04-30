3dd67315263291988a0bc916143b2393460607c7
protap_barman <protapbarman02@gmail.com>
2024-04-30 17:14:02 +0530

    service book, my orders

--- app/services/order_log_service.py
+++ app/services/order_log_service.py
@@ -21,27 +21,40 @@ class OrderLogService(BaseService):
 
         return order_logs_dicts
     
-    # def add_order_log_with_this(self, items: dict) -> dict:
+    def add_order_latest_log(self,orders):
+        for order in orders['data']:
+            order_log= self.model.query.filter_by(order_id=order['id']).order_by(self.model.created_at.desc()).first()
+            if order_log:
+                order['order_latest_log'] = {
+                    'order_log_id': order_log.id,
+                    'created_at': order_log.created_at, 
+                    'status':order_log.order_status,
+                    'status_name': order_statuses.get_value(order_log.order_status)
+                }
+        return orders
+
+    # def add_order_logs_with_this(self, items: dict) -> dict:
     #     for item in items["data"]:
     #         item["order_logs"] = self.get_order_log_by_order_id(item['id'])
+    #         print(item['order_logs'])
     #     return items
     
     # def get_order_log_by_order(self,order_id):
     #     return self.model.query.filter_by(id=order_id).all()
 
-    def add_order_logs_with_this(self,orders):
-        for order in orders['data']:
-            order_logs = self.model.query.filter_by(order_id=order['id']).order_by(self.model.created_at).all()
-            order_logs_dicts = []
-            if order_logs:
-                for log in order_logs:
-                    log_dict = {
-                        'log_id': log.id,
-                        'created_at': log.created_at,  # Convert datetime to string
-                        'status_key': log.order_status,  # Convert datetime to string
-                        'status_name': order_statuses.get_value(log.order_status)  # Add your additional key and value here
-                    }
-                    order_logs_dicts.append(log_dict)
-                order['order_logs']=order_logs_dicts
-        print(order['order_logs'])
-        return orders
+    # def add_order_logs_with_this(self,orders):
+    #     for order in orders['data']:
+    #         order_logs = self.model.query.filter_by(order_id=order['id']).order_by(self.model.created_at).all()
+    #         order_logs_dicts = []
+    #         if order_logs:
+    #             for log in order_logs:
+    #                 log_dict = {
+    #                     'log_id': log.id,
+    #                     'created_at': log.created_at,  # Convert datetime to string
+    #                     'status_key': log.order_status,  # Convert datetime to string
+    #                     'status_name': order_statuses.get_value(log.order_status)  # Add your additional key and value here
+    #                 }
+    #                 order_logs_dicts.update(log_dict)
+    #             order['order_logs']=order_logs_dicts
+    #     print(order['order_logs'])
+    #     return orders
